2a195fe96e3dc649e36f6ab2ec6c1d1586634a7d
Ali Jendoubi <alijandhd@gmail.com>
2022-11-21 14:32:30 +0100

    Install Tailwind & DaisyUi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App text-4xl text-red-500">
       Hello World
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+module.exports = {
+  content: ['./src/**/*.{jsx,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+};
