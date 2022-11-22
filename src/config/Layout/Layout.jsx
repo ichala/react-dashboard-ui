9809b7a9d755b4aa2effeb1ec33efab69e120c0f
Ali Jendoubi <alijandhd@gmail.com>
2022-11-22 17:32:15 +0100

    update Layout

--- src/config/Layout/Layout.jsx
+++ src/config/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../../Components/Sidebar/Sidebar';
 function Layout({ children }) {
   return (
     <>
-      <div className="bg-base-100 text-white ">
+      <div className="bg-base-200 text-white ">
         <Sidebar>
           {children}
         </Sidebar>
