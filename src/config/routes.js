24a01ff4d8310bd9be91a52a985cd3e553b9bc69
Ali Jendoubi <alijandhd@gmail.com>
2022-11-22 17:32:09 +0100

    Add users route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,15 @@
 import Home from '../Pages/Home/Home';
+import Users from '../Pages/Users/Users';
 
 const routesConfig = [
 
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/users',
+    element: <Users />,
   },
 
 ];
