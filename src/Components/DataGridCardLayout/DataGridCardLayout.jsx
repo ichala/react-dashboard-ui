06106869921645f10fbcc2b18026b001640ce94f
Ali Jendoubi <alijandhd@gmail.com>
2022-11-22 17:31:43 +0100

    Add Grid Layout

---
+++ src/Components/DataGridCardLayout/DataGridCardLayout.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import UserCard from '../Sidebar/Cards/UserCard';
+
+function DataGridCardLayout() {
+  return (
+    <div className="flex justify-center gap-5 flex-wrap items-center">
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+    </div>
+  );
+}
+
+export default DataGridCardLayout;
