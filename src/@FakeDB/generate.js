544459d3dae8350e381b46d5f80eb860abe0552c
Ali Jendoubi <alijandhd@gmail.com>
2022-11-23 12:24:53 +0100

    Implement fake Data

---
+++ src/@FakeDB/generate.js
@@ -0,0 +1,14 @@
+import { faker } from '@faker-js/faker';
+
+const createRandomUser = () => ({
+  _id: faker.datatype.uuid(),
+  avatar: faker.image.avatar(),
+  birthday: faker.date.birthdate(),
+  email: faker.internet.email(),
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  sex: faker.name.sexType(),
+  status: faker.helpers.arrayElement(['Active', 'Banned', 'Desactivated']),
+});
+
+export default createRandomUser;
--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  FiUserMinus, FiUserPlus, FiUsers, FiUserX,
+  FiUserMinus, FiUserPlus, FiUsers, FiUserX, FiPlusCircle,
 } from 'react-icons/fi';
 
 import {
@@ -9,9 +9,19 @@ import {
 import SmallStatsCard from '../../Components/Sidebar/Cards/SmallStatsCard';
 import DataGridCardLayout from '../../Components/Users/DataGridCardLayout/UserDataGridCardLayout';
 import UserDataTableLayout from '../../Components/Users/DataGridCardLayout/UserDataTableLayout';
+import createRandomUser from '../../@FakeDB/generate';
 
 function Users() {
   const [DataLayout, setDataLayout] = useState('Table');
+  const [Users, setUsers] = useState([]);
+  useEffect(() => {
+    const GeneratedUsers = [];
+    Array.from({ length: 10 }).forEach(() => {
+      GeneratedUsers.push(createRandomUser());
+    });
+    setUsers(GeneratedUsers);
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center text-base-content">
       <div className="flex  justify-center w-full items-center lg:flex-row flex-col gap-1 p-4 flex-wrap">
@@ -23,12 +33,15 @@ function Users() {
       <div className="card w-full bg-base-100 shadow-md">
         <div className="card-body">
           <div className="card-actions justify-between items-center px-5">
-            <button type="button" className="btn btn-primary btn-sm">Add New User</button>
+            <button type="button" className="btn btn-primary btn-sm gap-2">
+              <FiPlusCircle size={20} />
+              Add User
+            </button>
             <div className="form-control">
               <div className="input-group">
-                <input type="text" placeholder="Search users ..." className="input font-semibold md:text-2xl md:input-sm input-bordered  w-full max-w-xs " />
-                <select className="select select-bordered select-sm ">
-                  <option disabled selected>Filter By</option>
+                <input type="text" placeholder="Search users ..." className="input focus:outline-0 font-semibold md:text-2xl md:input-sm input-bordered  w-full max-w-xs " />
+                <select className="select select-bordered focus:outline-0 select-sm ">
+                  <option>Filter By</option>
                   <option>Han Solo</option>
                   <option>Greedo</option>
                 </select>
@@ -44,7 +57,7 @@ function Users() {
 
             </div>
           </div>
-          {DataLayout === 'Table' ? <UserDataTableLayout /> : <DataGridCardLayout />}
+          {DataLayout === 'Table' ? <UserDataTableLayout Users={Users} /> : <DataGridCardLayout Users={Users} />}
 
         </div>
       </div>
