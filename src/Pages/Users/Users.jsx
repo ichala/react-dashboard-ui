77ef3098e998102de771ed4ab2666b4ff88958bf
Ali Jendoubi <alijandhd@gmail.com>
2022-11-23 16:10:55 +0100

    Remove inline-style

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -97,8 +97,8 @@ function Users() {
                 </div>
               </div>
               <input type="checkbox" id="new-user-modal" className="modal-toggle" />
-              <div style={{ zIndex: '99999 !important;' }} className="modal ">
-                <div style={{ zIndex: '9999999 !important;' }} className="modal-box  max-w-5xl">
+              <div className="modal ">
+                <div className="modal-box  max-w-5xl">
                   <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
                   <p className="py-4">Youve been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                   <div className="modal-action">
