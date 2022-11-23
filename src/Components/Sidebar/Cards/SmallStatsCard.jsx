09a98c1aba144d322fb0f757585699fb0252b556
Ali Jendoubi <alijandhd@gmail.com>
2022-11-23 11:38:46 +0100

    Install Faker

--- src/Components/Sidebar/Cards/SmallStatsCard.jsx
+++ src/Components/Sidebar/Cards/SmallStatsCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { faker } from '@faker-js/faker';
 
 function SmallStatsCard({
   icon, title, stats, change,
 }) {
+  const user = {
+    _id: faker.datatype.uuid(),
+    avatar: faker.image.avatar(),
+    birthday: faker.date.birthdate(),
+    email: faker.internet.email(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    sex: faker.name.sexType(),
+    subscriptionTier: faker.helpers.arrayElement(['free', 'basic', 'business']),
+  };
+  console.log(user);
   return (
     <div className="card glass lg:w-[23%]   w-full bg-base-100 shadow-md">
       <div className="card-body p-2 flex flex-row justify-arround items-center">
