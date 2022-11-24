0ca01a25a63dc83dfa05e7abfdd4d53e4841b2ab
Ali Jendoubi <alijandhd@gmail.com>
2022-11-24 11:01:17 +0100

    Create theme Provider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './config/global.css';
 import Layout from './config/Layout/Layout';
+import { ThemeProvider } from './config/Context/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Layout>
-        <App />
-      </Layout>
+      <ThemeProvider>
+        <Layout>
+          <App />
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
